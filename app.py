82c3faa2f9ffc34b43d7152792c12583dc45dd6e
nekokaitaidesu-cpu <takeogonzaless@gmail.com>
2026-01-30 15:54:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # ページ設定
-st.set_page_config(page_title="Daifuku Cat Animation", page_icon="🍄")
+st.set_page_config(page_title="Daifuku Cat Animation Corrected", page_icon="🍄")
 
-st.title("もちもちだいふく猫だっち 🍄")
-st.write("HTMLとCSSだけで描画して動かしているっち！")
+st.title("もちもちだいふく猫だっち（修正版） 🍄")
+st.write("赤ペンの指示に合わせて、お顔の位置としっぽをフサフサに修正したっち！")
 
 # HTML/CSSコード
 html_code = """
@@ -25,8 +25,8 @@ html_code = """
   /* 猫の全体ラッパー */
   .cat-wrapper {
     position: relative;
-    width: 320px;
-    height: 200px;
+    width: 350px; /* しっぽのために少し広げる */
+    height: 220px; /* 少し高さを広げる */
   }
 
   /* 体（だいふく部分） */
@@ -38,50 +38,50 @@ html_code = """
     border: 4px solid #333;
     border-radius: 50% 50% 45% 45% / 60% 60% 40% 40%;
     z-index: 10;
-    top: 0;
+    top: 10px; /* 少し下げる */
     left: 0;
   }
 
-  /* 耳 */
+  /* 耳 (修正: 位置を少し外側に、形を少し尖らせる) */
   .ear {
     position: absolute;
-    width: 50px;
-    height: 50px;
+    width: 45px; /* 少し幅を狭く */
+    height: 45px; /* 少し高さを低く */
     background-color: #fff;
     border: 4px solid #333;
-    border-radius: 10px 40px 0 0;
+    border-radius: 10px 35px 0 0; /* 角丸を調整して尖らせる */
     z-index: 5;
   }
   .ear.left {
-    top: -15px;
-    left: 40px;
-    transform: rotate(-20deg);
+    top: 0px; /* 位置調整 */
+    left: 35px; /* 位置調整 */
+    transform: rotate(-25deg); /* 角度調整 */
   }
   .ear.right {
-    top: -15px;
-    left: 110px;
-    transform: rotate(10deg);
+    top: 0px; /* 位置調整 */
+    left: 120px; /* 位置調整 */
+    transform: rotate(15deg); /* 角度調整 */
   }
   /* 耳の内側の線を隠すためのカバー */
   .ear-cover {
     position: absolute;
-    width: 40px;
-    height: 10px;
+    width: 38px;
+    height: 15px;
     background-color: #fff;
     z-index: 11;
-    top: 25px;
-    left: 5px;
+    top: 28px;
+    left: 2px;
   }
 
-  /* 顔のパーツ */
+  /* 顔のパーツ (修正: 全体的に下に移動) */
   .face {
     position: absolute;
     z-index: 20;
-    top: 80px;
-    left: 40px;
+    top: 100px; /* 下に移動 */
+    left: 60px; /* 少し右に移動して中央寄せ */
   }
 
-  /* 目 (ニコニコ) */
+  /* 目 (修正: 間隔を少し狭める) */
   .eye {
     position: absolute;
     width: 20px;
@@ -91,9 +91,9 @@ html_code = """
     top: 0;
   }
   .eye.left { left: 0; }
-  .eye.right { left: 80px; }
+  .eye.right { left: 70px; } /* 間隔を狭める */
 
-  /* ほっぺ */
+  /* ほっぺ (修正: 位置を調整) */
   .cheek {
     position: absolute;
     width: 20px;
@@ -103,10 +103,10 @@ html_code = """
     opacity: 0.6;
     top: 20px;
   }
-  .cheek.left { left: -15px; }
-  .cheek.right { left: 95px; }
+  .cheek.left { left: -10px; } /* 位置調整 */
+  .cheek.right { left: 85px; } /* 位置調整 */
 
-  /* 口 (wの形) */
+  /* 口 (修正: 位置を調整) */
   .mouth {
     position: absolute;
     width: 20px;
@@ -116,7 +116,7 @@ html_code = """
     border-radius: 0 0 10px 0;
     transform: rotate(45deg);
     top: 15px;
-    left: 40px;
+    left: 35px; /* 目の間に合わせる */
   }
   .mouth::after {
     content: '';
@@ -131,27 +131,63 @@ html_code = """
     left: 0;
   }
   
-  /* しっぽ */
-  .tail {
+  /* しっぽ (修正: 複数の楕円を組み合わせてフサフサ感を出す) */
+  .tail-wrapper {
     position: absolute;
-    width: 100px;
-    height: 60px;
+    top: 70px;
+    right: -50px;
+    z-index: 1;
+    transform-origin: 0% 50%;
+    animation: wag 1s infinite alternate ease-in-out;
+  }
+  .tail-part {
+    position: absolute;
     background-color: #fff;
     border: 4px solid #333;
     border-radius: 50%;
-    top: 60px;
-    right: -40px;
-    z-index: 1;
-    transform-origin: 0% 50%; /* 左側を中心に回転 */
-    animation: wag 1s infinite alternate ease-in-out; /* アニメーション設定 */
   }
+  /* メインの房 */
+  .tail-main {
+    width: 100px;
+    height: 60px;
+    top: 0;
+    left: 0;
+    z-index: 3;
+  }
+  /* 上のフサフサ */
+  .tail-top {
+    width: 50px;
+    height: 40px;
+    top: -15px;
+    left: 40px;
+    transform: rotate(-20deg);
+    z-index: 2;
+  }
+  /* 下のフサフサ */
+  .tail-bottom {
+    width: 50px;
+    height: 40px;
+    top: 35px;
+    left: 30px;
+    transform: rotate(20deg);
+    z-index: 2;
+  }
+  /* 継ぎ目を隠すカバー */
+  .tail-cover {
+    position: absolute;
+    background-color: #fff;
+    z-index: 4;
+  }
+  .tail-cover-1 { width: 40px; height: 20px; top: 5px; left: 45px; transform: rotate(-10deg); }
+  .tail-cover-2 { width: 40px; height: 20px; top: 35px; left: 35px; transform: rotate(10deg); }
+
 
   /* ハート */
   .heart {
     position: absolute;
     color: #333;
     font-size: 24px;
-    top: 30px;
+    top: 40px; /* 少し下げる */
     right: -40px;
     z-index: 20;
     animation: float 2s infinite ease-in-out;
@@ -161,7 +197,7 @@ html_code = """
   /* アニメーション定義 */
   @keyframes wag {
     0% { transform: rotate(0deg); }
-    100% { transform: rotate(25deg); }
+    100% { transform: rotate(20deg); } /* 角度を少し控えめに */
   }
 
   @keyframes float {
@@ -178,7 +214,13 @@ html_code = """
     <div class="ear left"><div class="ear-cover"></div></div>
     <div class="ear right"><div class="ear-cover"></div></div>
     
-    <div class="tail"></div>
+    <div class="tail-wrapper">
+        <div class="tail-part tail-main"></div>
+        <div class="tail-part tail-top"></div>
+        <div class="tail-part tail-bottom"></div>
+        <div class="tail-cover tail-cover-1"></div>
+        <div class="tail-cover tail-cover-2"></div>
+    </div>
     
     <div class="body"></div>
     
